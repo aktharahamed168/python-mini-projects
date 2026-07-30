f74a5112ba93a2866988dc1578ca085f64ba3a54
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-07-30 10:57:33 +0530

    Implement age calculator using date of birth input

--- 09-age-calculator/main.py
+++ 09-age-calculator/main.py
@@ -1,1 +1,16 @@
+from datetime import datetime
 
+print("===== Age Calculator =====")
+
+dob = input("Enter your Date of Birth (DD-MM-YYYY): ")
+
+birth_date = datetime.strptime(dob, "%d-%m-%Y")
+
+today = datetime.today()
+
+age = today.year - birth_date.year
+
+if (today.month, today.day) < (birth_date.month, birth_date.day):
+    age -= 1
+
+print(f"\nYou are {age} years old.")

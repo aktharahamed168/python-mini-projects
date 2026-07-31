ffc7c76a426ef892f1f98104e5d324266cdd17f1
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-07-31 11:29:53 +0530

    Implement currency converter using API
    Fetch currency conversion rates from an API and calculate the converted amount based on user input.

--- 10-currency-converter/main.py
+++ 10-currency-converter/main.py
@@ -1,1 +1,23 @@
+import requests
 
+print("===== Currency Converter =====")
+
+amount = float(input("Enter Amount: "))
+from_currency = input("From Currency (e.g. USD): ").upper()
+to_currency = input("To Currency (e.g. INR): ").upper()
+
+url = f"https://open.er-api.com/v6/latest/{from_currency}"
+
+response = requests.get(url)
+data = response.json()
+
+if data["result"] == "success":
+    rate = data["rates"].get(to_currency)
+
+    if rate:
+        converted = amount * rate
+        print(f"\n{amount} {from_currency} = {converted:.2f} {to_currency}")
+    else:
+        print("Invalid target currency code.")
+else:
+    print("Invalid source currency code.")

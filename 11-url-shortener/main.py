b957ed4338d3fa8ecf35aecf3d5648e583e7307f
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-08-01 21:09:25 +0530

    Add URL shortener feature
    Implement URL shortening functionality using Clean URL API.

--- 11-url-shortener/main.py
+++ 11-url-shortener/main.py
@@ -1,1 +1,16 @@
+import requests
 
+print("===== URL Shortener =====")
+
+long_url = input("Enter Long URL: ")
+
+url = "https://cleanuri.com/api/v1/shorten"
+
+response = requests.post(url, data={"url": long_url})
+
+if response.status_code == 200:
+    data = response.json()
+    print("\nShort URL:")
+    print(data["result_url"])
+else:
+    print("Failed to shorten the URL.")

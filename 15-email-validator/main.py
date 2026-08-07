082c764784ee7dd841b755770874e054ed74a0dd
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-08-07 11:33:39 +0530

    Add email validation functionality

--- 15-email-validator/main.py
+++ 15-email-validator/main.py
@@ -1,1 +1,12 @@
+import re
 
+print("===== Email Validator =====")
+
+email = input("Enter Email Address: ")
+
+pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+
+if re.match(pattern, email):
+    print("\n✅ Valid Email Address")
+else:
+    print("\n❌ Invalid Email Address")

2229d12a95946c8049248ad73422f4eb79e1b2ad
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-08-15 22:12:11 +0530

    Implement JSON formatter function
    Add a JSON formatter function to read and print formatted JSON data from a file.

--- 23-json-formatter/main.py
+++ 23-json-formatter/main.py
@@ -1,1 +1,24 @@
+import json
 
+
+def format_json(filename):
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        print("\n===== Formatted JSON =====\n")
+
+        print(json.dumps(data, indent=4))
+
+    except FileNotFoundError:
+        print("File not found.")
+
+    except json.JSONDecodeError:
+        print("Invalid JSON format.")
+
+
+print("===== JSON Formatter =====")
+
+filename = "sample.json"
+
+format_json(filename)

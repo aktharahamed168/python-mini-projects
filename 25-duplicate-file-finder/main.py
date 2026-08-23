4b4cdf983645fddb634f2a9d6f5401be82cb4cc8
Akthar Ahamed <akthar.ahamed168@gmail.com>
2026-08-23 22:14:11 +0530

    Implement duplicate file finder using hashing

--- 25-duplicate-file-finder/main.py
+++ 25-duplicate-file-finder/main.py
@@ -1,1 +1,53 @@
+import os
+import hashlib
 
+
+def calculate_hash(file_path):
+    hasher = hashlib.md5()
+
+    with open(file_path, "rb") as file:
+        while chunk := file.read(4096):
+            hasher.update(chunk)
+
+    return hasher.hexdigest()
+
+
+def find_duplicates(folder):
+    hashes = {}
+    duplicates = []
+
+    for filename in os.listdir(folder):
+
+        file_path = os.path.join(folder, filename)
+
+        if not os.path.isfile(file_path):
+            continue
+
+        file_hash = calculate_hash(file_path)
+
+        if file_hash in hashes:
+            duplicates.append(
+                (hashes[file_hash], filename)
+            )
+        else:
+            hashes[file_hash] = filename
+
+    return duplicates
+
+
+print("===== Duplicate File Finder =====")
+
+folder = "sample_files"
+
+duplicates = find_duplicates(folder)
+
+print("\nDuplicates Found")
+print("-----------------")
+
+if duplicates:
+
+    for original, duplicate in duplicates:
+        print(f"{original} = {duplicate}")
+
+else:
+    print("No duplicate files found.")
